1daaa312b3ae150b93be2a351e6174bc6806d3c3
Anqi-Tech <71527898+Anqi-Tech@users.noreply.github.com>
2021-05-09 22:40:47 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,20 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "u8BKHxa00AgeWIPATsMKvkhaqugW80M2";
+
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${key}`
+    );
+    const data = await response.json();
+    data.results.forEach((book) => {
+      DOMSelectors.grid.insertAdjacentHTML("beforeend");
+    });
+  } catch (error) {
+    console.log(error);
+    alert("There's something wrong.");
+  }
+};
+query();
