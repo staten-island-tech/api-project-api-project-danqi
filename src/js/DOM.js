edc8ae4bf80e3660253e31f8147b6bfa78487e91
Anqi-Tech <71527898+Anqi-Tech@users.noreply.github.com>
2021-05-26 00:03:13 -0400

    Need to fix fetch API

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,6 +1,9 @@
 const DOMSelectors = {
   grid: document.querySelector(".book-grid"),
   sectionFiction: document.querySelector(".section-fiction"),
+  sectionNonfiction: document.querySelector(".section-nonfiction"),
+  sectionYoungAdult: document.querySelector(".section-ya"),
+  sectionChildren: document.querySelector(".section-children"),
   sectionManga: document.querySelector(".section-manga"),
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -4,16 +4,37 @@ import { DOMSelectors } from "./DOM";
 const key = "u8BKHxa00AgeWIPATsMKvkhaqugW80M2";
 
 const buttonFiction = document.getElementById("btn-fiction");
+const buttonNonFiction = document.getElementById("btn-fiction");
+const buttonYoungAdult = document.getElementById("btn-ya");
+const buttonChildren = document.getElementById("btn-children");
 const buttonManga = document.getElementById("btn-manga");
 
 const rankingsFiction = document.getElementById("section-fiction");
+const rankingsNonfiction = document.getElementById("section-nonfiction");
+const rankingsYoungAdult = document.getElementById("section-ya");
+const rankingsChildren = document.getElementById("section-children");
 const rankingsManga = document.getElementById("section-manga");
 
 buttonFiction.addEventListener("click", showFiction);
+buttonNonFiction.addEventListener("click", showNonfiction);
+buttonYoungAdult.addEventListener("click", showYoungAdult);
+buttonChildren.addEventListener("click", showChildren);
 buttonManga.addEventListener("click", showManga);
 
 function showFiction() {
   rankingsFiction.classList.remove("hide");
+  rankingsManga.classList.add("hide");
+}
+function showNonfiction() {
+  rankingsNonfiction.classList.remove("hide");
+  rankingsManga.classList.add("hide");
+}
+function showYoungAdult() {
+  rankingsYoungAdult.classList.remove("hide");
+  rankingsManga.classList.add("hide");
+}
+function showChildren() {
+  rankingsChildren.classList.remove("hide");
   rankingsManga.classList.add("hide");
 }
 function showManga() {
@@ -45,7 +66,6 @@ const sectionFiction = async function () {
             <p class="user-rank">Ranking</p>
             <p class="user-rank">${book.rank}</p>
           </div>
-
           <div class="author-box">
             <p class="author-book">Author</p>
             <p class="author-book">${book.author}</p>
@@ -56,10 +76,127 @@ const sectionFiction = async function () {
     });
   } catch (error) {
     console.log(error);
-    alert("There's something wrong.");
+    alert("There's something wrong with the fiction section.");
   }
 };
 sectionFiction();
+
+const sectionNonfiction = async function () {
+  try {
+    const response = await fetch();
+    `https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-nonfiction.json?api-key=${key}`;
+    const data = await response.json();
+    //console.log(data.results.books);
+    const books = data.results.books;
+    books.forEach((book) => {
+      DOMSelectors.sectionNonfiction.insertAdjacentHTML(
+        "beforeend",
+        `<div class="book-card">
+        <div class="book-card-front">
+          <img
+            src= ${book.book_image}
+            alt=""
+            class="cover"
+          />
+        </div>
+        <div class="book-card-back">
+          <h3 class="book-card-header">${book.title}</h3>
+          <div class="rank-box">
+            <p class="user-rank">Ranking</p>
+            <p class="user-rank">${book.rank}</p>
+          </div>
+          <div class="author-box">
+            <p class="author-book">Author</p>
+            <p class="author-book">${book.author}</p>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("There's something wrong with the nonfiction section.");
+  }
+};
+sectionNonfiction();
+
+const sectionYoungAdult = async function () {
+  try {
+    const response = await fetch();
+    `https://api.nytimes.com/svc/books/v3/lists/young-adult.json?api-key=${key}`;
+    const data = await response.json();
+    //console.log(data.results.books);
+    const books = data.results.books;
+    books.forEach((book) => {
+      DOMSelectors.sectionYoungAdult.insertAdjacentHTML(
+        "beforeend",
+        `<div class="book-card">
+        <div class="book-card-front">
+          <img
+            src= ${book.book_image}
+            alt=""
+            class="cover"
+          />
+        </div>
+        <div class="book-card-back">
+          <h3 class="book-card-header">${book.title}</h3>
+          <div class="rank-box">
+            <p class="user-rank">Ranking</p>
+            <p class="user-rank">${book.rank}</p>
+          </div>
+          <div class="author-box">
+            <p class="author-book">Author</p>
+            <p class="author-book">${book.author}</p>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("There's something wrong with the yound adult section.");
+  }
+};
+sectionYoungAdult();
+
+const sectionChildren = async function () {
+  try {
+    const response = await fetch();
+    `https://api.nytimes.com/svc/books/v3/lists/picture-books.json?api-key=${key}`;
+    const data = await response.json();
+    //console.log(data.results.books);
+    const books = data.results.books;
+    books.forEach((book) => {
+      DOMSelectors.sectionChildren.insertAdjacentHTML(
+        "beforeend",
+        `<div class="book-card">
+        <div class="book-card-front">
+          <img
+            src= ${book.book_image}
+            alt=""
+            class="cover"
+          />
+        </div>
+        <div class="book-card-back">
+          <h3 class="book-card-header">${book.title}</h3>
+          <div class="rank-box">
+            <p class="user-rank">Ranking</p>
+            <p class="user-rank">${book.rank}</p>
+          </div>
+          <div class="author-box">
+            <p class="author-book">Author</p>
+            <p class="author-book">${book.author}</p>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("There's something wrong with the children section.");
+  }
+};
+sectionChildren();
 
 const sectionManga = async function () {
   try {
@@ -67,7 +204,6 @@ const sectionManga = async function () {
       `https://api.nytimes.com/svc/books/v3/lists/manga.json?api-key=${key}`
     );
     const data = await response.json();
-    //console.log(data.results.books);
     const books = data.results.books;
     books.forEach((book) => {
       DOMSelectors.sectionManga.insertAdjacentHTML(
@@ -86,7 +222,6 @@ const sectionManga = async function () {
             <p class="user-rank">Ranking</p>
             <p class="user-rank">${book.rank}</p>
           </div>
-
           <div class="author-box">
             <p class="author-book">Author</p>
             <p class="author-book">${book.author}</p>
@@ -97,11 +232,7 @@ const sectionManga = async function () {
     });
   } catch (error) {
     console.log(error);
-    alert("There's something wrong.");
+    alert("There's something wrong with the manga section.");
   }
 };
 sectionManga();
-
-/*function showFiction() {
-      visibility.classList.remove("hide");
-    }*/
